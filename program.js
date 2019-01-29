88cc83015ecd352d77e6e7dff6c84bc95a75deff
Andrzej Grabowski <andgrabowski@gmail.com>
2019-01-29 23:39:00 +0100

    Add switch statement

--- program.js
+++ program.js
@@ -4,11 +4,29 @@ process.stdin.on('readable', function () {
     var input = process.stdin.read();
     if (input !== null) {
         var instruction = input.toString().trim();
-        if (instruction === '/exit') {
-            process.stdout.write('Quitting app!\n');
-            process.exit();
-        } else {
-            process.stderr.write('Wrong instruction!\n');
+        switch (instruction) {
+            case '/exit':
+                process.stdout.write('Quitting app!\n');
+                process.exit();
+                break;
+            case 'ver':
+                process.stdout.write(`Version: ${process.version}`);
+                break;
+            case 'lang':
+                process.stdout.write(`Language: ${process.env.lang}`);
+                break;
+            default:
+                process.stderr.write('Wrong instruction!\n');
+
         }
+
+        // if (instruction === '/exit') {
+        //     process.stdout.write('Quitting app!\n');
+        //     process.exit();
+        // } else {
+        //     process.stderr.write('Wrong instruction!\n');
+        //     console.log(`Version: ${process.version}`);
+        //     console.log(`Language: ${process.env.lang}`);
+        // }
     }
 });
